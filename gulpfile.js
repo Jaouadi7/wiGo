d369076f298f197755a8e76469a9367695e83266
Mohamed A. Eljaouadi <jaouadi@outlook.com>
2022-10-12 23:01:11 +0100

    setup browsersync server

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 //---------------------------------------
-//     SETUP THE PROJECT ROUTES       ---
-//---------------------------------------
-
-const development = './src/';
-const production = './dist/';
-const node_modules = './node_modules/';
-
-//---------------------------------------
 //        INSTALLED NPM PLUGINS       ---
 //---------------------------------------
 
 const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+import options from './config';
+
+//---------------------------------------
+//     SETUP THE PROJECT ROUTES       ---
+//---------------------------------------
+
+const development = options.paths.src.base;
+const production = options.paths.dist.base;
+const node_modules = './node_modules/';
+
+//----------------------------------------------
+//     SETUP THE PROJECT STATIC SERVER       ---
+//----------------------------------------------
+
+// START THE DEVELOPEMNT STATIC SERVER
+const start_server = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: production,
+    },
+    port: options.config.port || 5000,
+  });
+};
+
+// RELOAD THE BROWSER
+const reload = (done) => {
+  browserSync.reload;
+  done();
+};
